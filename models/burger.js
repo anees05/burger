19ff9dae201005f2203920618c38aade8b5eccbf
anees05 <anees.adams05@gmail.com>
2019-08-13 19:41:38 -0400

    Set up model

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+var orm = require("../config/orm")
+
+var burger = {
+    all: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res)
+        });
+    },
+
+    insert: function (val, cb) {
+        orm.insert("burgers", "burger_name", val, function (res) {
+            cb(res);
+        });
+    },
+
+    update: function (col, conditional, cb) {
+        orm.update("burgers", col, conditional, function (res) {
+            cb(res)
+        })
+    }
+};
+
+module.exports = burger;
