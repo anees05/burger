7d2e740e6f2b514424c9667227c00a9178b7e7f0
anees05 <anees.adams05@gmail.com>
2019-08-13 19:37:21 -0400

    Update controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,24 +4,24 @@ var router = express.Router();
 var burger = require("../models/burger");
 
 router.get("/", function (req, res) {
-  burger.selectAll(function (data) {
-    res.render("index", { burgers: data })
-  })
+    burger.all(function (data) {
+        res.render("index", { burgers: data })
+    })
 })
 
 router.put("/api/burgers/:id", function (req, res) {
-  var col = "devoured = " + req.body.devoured;
-  var cond = "id = " + req.params.id;
+    var col = "devoured = " + req.body.devoured;
+    var condition = "id = " + req.params.id;
 
-  burger.updateOne(col, cond, function (result) {
-    res.json({ id: result.insertId })
-  })
+    burger.update(col, condition, function (result) {
+        res.json({ id: result.insertId })
+    })
 })
 
 router.post("/api/burgers", function (req, res) {
-  burger.insertOne(req.body.name, function (result) {
-    res.json({ id: result.insertId })
-  })
+    burger.insert(req.body.name, function (result) {
+        res.json({ id: result.insertId })
+    })
 })
 
 module.exports = router;
