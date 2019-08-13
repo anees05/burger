a7fbcd5e411b93c0eaaf8cd285c931c50449d410
anees05 <anees.adams05@gmail.com>
2019-08-13 19:20:05 -0400

    Create info for db

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+    VALUES ("black bean burger", false),
+           ("bison burger", false),
+           ("turkey burger", false),
+           ("cheese burger", false),
+           ("good-burger burger", false);
