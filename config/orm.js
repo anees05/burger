c6e260657ec561e09af58ec642decafa44de619d
anees05 <anees.adams05@gmail.com>
2019-08-14 09:41:02 -0400

    Fix issues with functions

--- config/orm.js
+++ config/orm.js
@@ -1,25 +1,25 @@
 var connection = require("./connection")
 
 var orm = {
-  selectAll: function(table, cb) {
+  all: function (table, cb) {
     var queryString = "SELECT * FROM " + table + ";";
-    connection.query(queryString, function(err, res) {
+    connection.query(queryString, function (err, res) {
       if (err) throw err;
       cb(res);
     });
   },
 
-  insertOne: function(table, col, val, cb) {
+  insert: function (table, col, val, cb) {
     var queryString = "INSERT INTO " + table + " (" + col + ") VALUES (?)";
-    connection.query(queryString, val, function(err, res) {
+    connection.query(queryString, val, function (err, res) {
       if (err) throw err;
       cb(res);
     })
   },
 
-  updateOne: function(table, col, conditional, cb) {
+  update: function (table, col, conditional, cb) {
     var queryString = "UPDATE " + table + " SET " + col + " WHERE " + conditional;
-    connection.query(queryStr, function(err, res) {
+    connection.query(queryStr, function (err, res) {
       if (err) throw err;
       cb(res)
     })
